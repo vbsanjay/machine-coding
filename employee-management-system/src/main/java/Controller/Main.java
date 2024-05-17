f8c3ef71dbfc4eda27540a602c66c783ac7bf722
vbsanjay <72456646+vbsanjay@users.noreply.github.com>
2024-05-17 13:30:10 +0530

    initial setup

---
+++ employee-management-system/src/main/java/Controller/Main.java
@@ -0,0 +1,7 @@
+package Controller;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("------ Welcome to Employee Management System ------");
+    }
+}
